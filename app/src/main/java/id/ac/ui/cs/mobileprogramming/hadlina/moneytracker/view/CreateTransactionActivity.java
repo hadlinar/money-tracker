dfe351e57adab948c2b8f91fa75ebb5c29913625
hadlinar <hadlinar@gmail.com>
2021-01-02 19:06:46 +0700

    fix bug camera permission issue

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/hadlina/moneytracker/view/CreateTransactionActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/hadlina/moneytracker/view/CreateTransactionActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,7 +16,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -45,65 +48,30 @@ public class CreateTransactionActivity extends AppCompatActivity{
     private ActivityTransactionCreateBinding binding;
     private TransactionViewModel viewModel;
     private SharedPreferences preferences;
-
-    public static final int REQUEST_CAMERA = 10;
-    public static final int WRITE_CAMERA = 11;
-    public static final int CAPTURE_CAMERA = 12;
-
-    public static final int REQUEST_GALLERY = 13;
-    public static final int WRITE_GALLERY = 14;
-    public static final int CAPTURE_GALLERY = 15;
-
-    private BottomSheetBehavior selectPhotoBehaviour;
-    private SimpleDraweeView swView;
-
-    public File filen = null;
+    Button btnTakePhoto;
+    ImageView imageView;
+    public static final int RequestPermissionCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_create);
 
-        LinearLayout llSelect = findViewById(R.id.select_photo);
-        selectPhotoBehaviour = BottomSheetBehavior.from(llSelect);
-
-        swView = findViewById(R.id.img1);
-
-        swView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(selectPhotoBehaviour.getState() != BottomSheetBehavior.STATE_EXPANDED){
-                    selectPhotoBehaviour.setState(BottomSheetBehavior.STATE_EXPANDED);
-                } else {
-                    selectPhotoBehaviour.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                }
-            }
-        });
-
-        TextView txtcamera = findViewById(R.id.dlg_camera);
-        TextView txtgallery = findViewById(R.id.dlg_gallery);
-
-        txtcamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectPhotoBehaviour.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                checkPermissionCW();
-            }
-        });
-
-        txtgallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectPhotoBehaviour.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                checkPermissionRG();
-            }
-        });
-
         preferences = getSharedPreferences(getString(R.string.preferences), Context.MODE_PRIVATE);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_transaction_create);
         viewModel = ViewModelProviders.of(this).get(TransactionViewModel.class);
         viewModel.email.setValue(preferences.getString("email", ""));
         viewModel.negativeBalance.setValue(false);
+        btnTakePhoto = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+        EnableRuntimePermission();
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 7);
+            }
+        });
 
         binding.setViewModel(viewModel);
 
@@ -131,147 +99,42 @@ public class CreateTransactionActivity extends AppCompatActivity{
         observe();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public void EnableRuntimePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(CreateTransactionActivity.this,
+                Manifest.permission.CAMERA)) {
+            Toast.makeText(CreateTransactionActivity.this,"access camera permission", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(CreateTransactionActivity.this,new String[]{
+                    Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] result) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (result.length > 0 && result[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(CreateTransactionActivity.this, "Permission Granted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(CreateTransactionActivity.this, "Can't access camera", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
     private boolean isFinished() {
         return preferences.getBoolean("timeout", false);
     }
 
-    private void checkPermissionCA(){
-        int permissionCheck = ContextCompat.checkSelfPermission(CreateTransactionActivity.this, Manifest.permission.CAMERA);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    CreateTransactionActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
-        } else {
-            catchPhoto();
-        }
-    }
-    private void checkPermissionCW(){
-        int permissionCheck = ContextCompat.checkSelfPermission(CreateTransactionActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    CreateTransactionActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_CAMERA);
-        } else {
-            checkPermissionCA();
-        }
-    }
-
-    private void catchPhoto() {
-        filen = getFile();
-        if(filen!=null) {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            try {
-                Uri photocUri = Uri.fromFile(filen);
-                intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photocUri);
-                startActivityForResult(intent, CAPTURE_CAMERA);
-            } catch (ActivityNotFoundException e) {
-
-            }
-        } else {
-            Toast.makeText(CreateTransactionActivity.this, "please check your sdcard status", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void checkPermissionRG(){
-        int permissionCheck = ContextCompat.checkSelfPermission(CreateTransactionActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    CreateTransactionActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_GALLERY);
-        } else {
-            checkPermissionWG();
-        }
-    }
-    private void checkPermissionWG(){
-        int permissionCheck = ContextCompat.checkSelfPermission(CreateTransactionActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        // int permissionCheck2 = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    CreateTransactionActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_GALLERY);
-        } else {
-            getPhotos();
-        }
-    }
-    private void getPhotos() {
-        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-        photoPickerIntent.setType("image/*");
-        startActivityForResult(photoPickerIntent, REQUEST_GALLERY);
-    }
-
-    public File getFile(){
-
-        File fileDir = new File(Environment.getExternalStorageDirectory()
-                + "/Android/data/"
-                + getApplicationContext().getPackageName()
-                + "/Files");
-
-        if (!fileDir.exists()){
-            if (!fileDir.mkdirs()){
-                return null;
-            }
-        }
-
-
-        File mediaFile = new File(fileDir.getPath() + File.separator + "temp.jpg");
-        return mediaFile;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_OK) {
-            Log.e("msg", "photo not get");
-            return;
-        }
-
-        switch (requestCode) {
-
-            case CAPTURE_CAMERA:
-
-                swView.setImageURI(Uri.parse("file:///" + filen));
-                break;
-
-
-            case REQUEST_GALLERY:
-                try {
-
-                    InputStream inputStream = getContentResolver().openInputStream(data.getData());
-                    filen = getFile();
-                    FileOutputStream fileOutputStream = new FileOutputStream(filen);
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        fileOutputStream.write(buffer, 0, bytesRead);
-                    }
-                    fileOutputStream.close();
-                    inputStream.close();
-                    swView.setImageURI(Uri.parse("file:///" + filen));//fresco library
-
-                } catch (Exception e) {
-
-                    Log.e("", "Error while creating temp file", e);
-                }
-                break;
-
-        }
-    }
-
-    public void onRequestPermissionsResult (int requestCode, String[] permissions,  int[] grantResults)
-    {
-
-        switch (requestCode) {
-            case REQUEST_CAMERA:
-                catchPhoto();
-                break;
-            case WRITE_CAMERA:
-                checkPermissionCA();
-                break;
-            case REQUEST_GALLERY:
-                checkPermissionWG();
-                break;
-            case WRITE_GALLERY:
-                getPhotos();
-                break;
-        }
-    }
 
     @Override
     protected void onResume() {
